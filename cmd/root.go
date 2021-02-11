9d5c8b23eb8ac56be7cc6f7ae6157aff030d2b73
t1732 <oo1732oo@gmail.com>
2021-02-12 00:29:45 +0900

    [feat] version command 実装

---
+++ cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "kumade",
+		Short: "Kumade is a comand for ConoHa VPS API",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/kumade/config)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home + ".config/kumade/")
+		viper.AddConfigPath("/etc/kumade/")
+		viper.SetConfigName("config")
+	}
+
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/cloudfoundry/bytefmt"
-	"github.com/olekukonko/tablewriter"
 	"github.com/t1732/kumade/cmd"
 )
 
 func main() {
-	token := cmd.GetToken()
-	images := cmd.GetVPCImages(token.ID)
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"ID", "Name", "Status", "Size", "CreatedAt"})
-	for _, img := range *images {
-		table.Append([]string{img.ID, img.Name, img.Status, bytefmt.ByteSize(uint64(img.Size)), img.CreatedAt.String()})
-	}
-	table.Render()
+	cmd.Execute()
 }
